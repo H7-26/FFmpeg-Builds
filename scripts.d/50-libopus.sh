79c2e13e3cbdd32a1d64f501878ced38cf5c3b45
H7-26 <92584178+H7-26@users.noreply.github.com>
2026-08-31 01:23:18 +0200

    rebase

--- scripts.d/50-libopus.sh
+++ scripts.d/50-libopus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/xiph/opus.git"
-SCRIPT_COMMIT="3da9f7a6db1c05c3996cb363a9d1931a978bf1be"
+SCRIPT_COMMIT="228a0f855ce24284e47866e30a66407202446a3a"
 
 ffbuild_enabled() {
     return 0
